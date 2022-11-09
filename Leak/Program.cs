71411390556dacf9dfbd2dedfe6f61e682b80bcf
Jannick Leismann <jannick.leismann@gmx.de>
2022-11-09 12:07:45 +0100

    initial project commit

---
+++ Leak/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static readonly List<string> list = new();
+
+    private static void Main()
+    {
+        while (true)
+        {
+            list.Add(GenRandomStr(10000));
+            Thread.Sleep(1);
+        }
+    }
+
+    static readonly Random rnd = new();
+
+    static string GenRandomStr(int length)
+    {
+        var chars = new List<char>();
+
+        for (var i = 0; i < length; i++)
+        {
+            var a = (char)rnd.Next(65, 122);
+            chars.Add(a);
+        }
+
+        return string.Concat(chars);
+    }
+}
